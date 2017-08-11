5248145f19c3dc19d6909e0b06d8c8b8cc7315f9
Pavel Astakhov <pastakhov@yandex.ru>
2017-08-11 15:17:43 +0600

    Optimize 2.4.5 container

--- 2.4.5/run-elasticsearch.sh
+++ 2.4.5/run-elasticsearch.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 set -e
 
-mkdir -p $ES_LOG_DIR
-mkdir -p $ES_LOG_DIR
+mkdir -p ${ES_VOLUME}/data
+mkdir -p ${ES_VOLUME}/logs
 
-OPTS="$OPTS -Des.default.path.logs=$ES_LOG_DIR -Des.default.path.data=$ES_DATA_DIR -Des.default.network.host=0.0.0.0"
+OPTS="${OPTS} \
+-Des.default.network.host=0.0.0.0 \
+-Des.default.path.data=${ES_VOLUME}/data \
+-Des.default.path.logs=${ES_VOLUME}/logs \
+-Des.default.rootLogger=${ES_LOGGER_LEVEL},\ console \
+-Des.default.logger.deprecation=INFO,\ console \
+-Des.default.logger.index.search.slowlog=TRACE,\ console \
+-Des.default.logger.index.indexing.slowlog=TRACE,\ console"
 
-echo "Starting Elasticsearch with the options $OPTS"
-CMD="$ES_HOME/bin/elasticsearch $OPTS"
+echo "Starting Elasticsearch with the options ${OPTS}"
+CMD="${ES_HOME}/bin/elasticsearch ${OPTS}"
 if [ `id -u` = 0 ]; then
   echo "Running as non-root..."
-  chown -R $ES_USER:$ES_USER $ES_VOLUME
-  su -c "$CMD" $ES_USER
+  chown -R ${ES_USER}:${ES_USER} ${ES_VOLUME}
+  su -c "${CMD}" ${ES_USER}
 else
-  $CMD
+  ${CMD}
 fi
